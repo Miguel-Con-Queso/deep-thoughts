ad018b07544999d4b1aeb755e7e022c0246b8f85
MiguelConQueso <michael_pnkstn@yahoo.com>
2021-09-01 16:21:19 -0400

    finished 21.3

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,64 +1,53 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
 
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-        reactions: [Reaction]
-    }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
 
-    type Reaction {
-        _id: ID
-        reactionBody: String
-        createdAt: String
-        username: String
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
-    type User {
-        _id: ID
-        username: String
-        email: String
-        friendCount: Int
-        thoughts: [Thought]
-        friends: [User]
-    }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
 
-    type Query {
-        me: User
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 
-<<<<<<< HEAD
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        addThought(thoughtId: ID!, reactionBody: String!): Thought
-        addFriend(friendId: ID!): User
-    }
-
-=======
->>>>>>> 1e6f81d94af45016e685cdd0eb994cd3b59fa942
-    type Auth {
-        token: ID!
-        user: User
-    }
-<<<<<<< HEAD
-=======
-
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-    }
->>>>>>> 1e6f81d94af45016e685cdd0eb994cd3b59fa942
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 // export the typeDefs
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-// import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-// import Middleware
-const { authMiddleware } = require('./utils/auth');
+// import path
+const path = require("path");
 
-const { authMiddleware } = require('./utils/auth');
+// impoort express
+const express = require("express");
+// import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+// import Middleware
+const { authMiddleware } = require("./utils/auth");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,7 +18,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 // integrate our Apollo server with the Express application as middleware
@@ -27,7 +29,16 @@ server.start().then(() => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
